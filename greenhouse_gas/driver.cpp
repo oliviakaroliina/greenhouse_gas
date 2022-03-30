ec01a2c7ebf07248d959c6b41d3ca2e7852af17a
Olivia Saukonoja <olivia.saukonoja@tuni.fi>
2022-03-30 09:47:31 +0300

    korjattu ohjelman crashaus poistamalla driverista api jutut

--- greenhouse_gas/driver.cpp
+++ greenhouse_gas/driver.cpp
@@ -2,43 +2,17 @@
 
 #include <QDebug>
 #include <QApplication>
-#include <QGuiApplication>
-#include <QQmlApplicationEngine>
-#include <QQmlContext>
-#include <memory>
-#include <iostream>
 
 driver::driver(int argc, char *argv[])
 {
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-#endif
-
     QApplication a(argc, argv);
     MainWindow w;
-    w.show();
-
     QObject::connect(&w,&MainWindow::visualizeButtonPressed,this,&driver::createApis);
+
     errorHandler errorhandler;
 
+    w.show();
     a.exec();
-
-    // Api jutut
-    QGuiApplication app(argc, argv);
-
-    QQmlApplicationEngine engine;
-    std::shared_ptr<statfiApi> handler{ std::make_shared<statfiApi>() };
-    engine.rootContext()->setContextProperty("networkHandler", handler.get());
-
-    const QUrl url(QStringLiteral("https://pxnet2.stat.fi/PXWeb/api/v1/en/ymp/taulukot/Kokodata.px"));
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url](QObject *obj, const QUrl &objUrl) {
-        if (!obj && url == objUrl)
-            QCoreApplication::exit(-1);
-    }, Qt::QueuedConnection);
-    engine.load(url);
-
-    app.exec();
 }
 
 void driver::createApis()
