6838d79c3f8b343aadb69c046a1a6b305a5b2715
Sini Kivioja <sini.kivioja@tuni.fi>
2022-04-07 20:00:03 +0300

    Modify statfi api

--- greenhouse_gas/statfiapi.cpp
+++ greenhouse_gas/statfiapi.cpp
@@ -10,7 +10,7 @@ statfiApi::statfiApi(QObject *parent, MainWindow* mw) :
     connect(manager_, &QNetworkAccessManager::finished, this,
             &statfiApi::downloadCompleted);
     mw_ = mw;
-    fetch();
+    getUsersSelections();
 }
 
 statfiApi::~statfiApi()
@@ -18,10 +18,16 @@ statfiApi::~statfiApi()
     delete manager_;
 }
 
-void statfiApi::fetch()
+QVector<QString> statfiApi::getResponse()
+{
+    return response_;
+}
+
+void statfiApi::fetch(QString start, QString end, QString coType)
 {
     QString url = "https://pxnet2.stat.fi:443/PXWeb/api/v1/en/ymp/taulukot/"
                   "Kokodata.px";
+
     QNetworkRequest request(url);
     request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
 
@@ -35,6 +41,7 @@ void statfiApi::fetch()
     gasSelectionObject.insert("filter", "item");
 
     QJsonArray datasetValuesArray;
+    // datasetValuesArray.push_back(coType);
     datasetValuesArray.push_back("Khk_yht");
     gasSelectionObject.insert("values", datasetValuesArray);
 
@@ -50,6 +57,21 @@ void statfiApi::fetch()
     yearSelectionObject.insert("filter", "item");
 
     QJsonArray yearValuesArray;
+    /* if end - start < 2:
+     yearValuesArray.push_back(start);
+     yearValuesArray.push_back(end);
+
+     else if end start < 1:
+     yearValuesArray.push_back(start);
+
+     else:
+     int years = end - start;
+     int n = 0;
+     while (n < years) {
+     addYear = start + n
+     yearValuesArray.push_back(addYear);
+     */
+
     yearValuesArray.push_back("2008");
     yearSelectionObject.insert("values", yearValuesArray);
 
@@ -65,8 +87,51 @@ void statfiApi::fetch()
     manager_->post(request, data);
 }
 
+void statfiApi::getUsersSelections()
+{
+    QVector<QString> coTypes = mw_->getDatasets();
+
+    bool in_tonnes = std::find(coTypes.begin(), coTypes.end(), IN_TONNES) != coTypes.end();
+    bool intensity = std::find(coTypes.begin(), coTypes.end(), INTENSITY) != coTypes.end();
+    bool intensity_indexed = std::find(coTypes.begin(), coTypes.end(), INTENSITY_INDEXED) != coTypes.end();
+    bool indexed = std::find(coTypes.begin(), coTypes.end(), INDEXED) != coTypes.end();
+
+    QString startDate = mw_->getStatfiStartDate().toString(Qt::ISODate);
+    QString endDate = mw_->getStatfiEndDate().toString(Qt::ISODate);
+
+    // CO2 emissions in 1000kgs
+    if (in_tonnes) {
+        fetch(startDate, endDate, IN_TONNES);
+        selections_++;
+    }
+
+    // CO2 emissions indexed
+    if (intensity) {
+        fetch(startDate, endDate, INTENSITY);
+        selections_++;
+    }
+
+    // CO2 emissions intensity, indexed
+    if (intensity_indexed) {
+        fetch(startDate, endDate, INTENSITY_INDEXED);
+        selections_++;
+    }
+
+    // CO2 emissions indexed
+    if (indexed) {
+        fetch(startDate, endDate, INDEXED);
+        selections_++;
+    }
+
+}
+
 void statfiApi::downloadCompleted(QNetworkReply *networkReply)
 {
-    //QString response = networkReply->readAll();
+    response_.push_back(networkReply->readAll());
     networkReply->deleteLater();
+
+    if (response_.size() == selections_) {
+        emit dataCollected();
+    }
+
 }
--- greenhouse_gas/statfiapi.hh
+++ greenhouse_gas/statfiapi.hh
@@ -15,14 +15,32 @@ public:
     explicit statfiApi(QObject* parent = nullptr, MainWindow* mw = nullptr);
     ~statfiApi();
 
+    QVector<QString> getResponse();
+
+signals:
+    void dataCollected();
+
 private Q_SLOTS:
     void downloadCompleted(QNetworkReply* networkReply);
 
 private:
     QNetworkAccessManager* manager_;
-    MainWindow* mw_;
+    MainWindow* mw_ = nullptr;
+    QVector<QString> response_ = {};
 
-    void fetch();
+    void fetch(QString start, QString end, QString coType);
+    void getUsersSelections();
+
+    int selections_ = 0;
+
+    const QString IN_TONNES = "in tonnes";
+    const QString INTENSITY = "intensity";
+    const QString INDEXED = "indexed";
+    const QString INTENSITY_INDEXED = "intensity indexed";
+    const QString API_IN_TONNES = "Khk_yht";
+    const QString API_INTENSITY = "Khk_yht_las";
+    const QString API_INDEXED = "Khk_yht_index";
+    const QString API_INTENSITY_INDEXED = "iKhk_yht_las_index";
 };
 
 #endif // STATFIAPI_HH
